7150589db4e1f15f9f6699220df9564c1ceeb1f4
ouvic <ouvic215@gmail.com>
2024-10-23 23:39:53 +0800

    update monitor.js

--- ecoweb/blog/static/blog/js/monitor.js
+++ ecoweb/blog/static/blog/js/monitor.js
@@ -126,21 +126,20 @@ $(document).ready(function () {
                             <div id="bollinger-chart" style="height: 400px;"></div>
                             <div id="profit-chart" style="height: 400px;"></div>
                             <div class="mt-4">
-                                <h6>交易訊號詳情</h6>
-                                <table id="signal-table" class="table table-striped table-bordered">
+                                <h6>Details</h6>
+                                    <table id="signal-table" class="table table-bordered table-striped">
                                     <thead>
                                         <tr>
-                                            <th>日期</th>
-                                            <th>類型</th>
-                                            <th>${trackDetails.stock1} 動作</th>
-                                            <th>${trackDetails.stock1} 價格</th>
-                                            <th>${trackDetails.stock2} 動作</th>
-                                            <th>${trackDetails.stock2} 價格</th>
+                                        <th>Date</th>
+                                        <th>Type</th>
+                                        <th>Action of AAPL</th>
+                                        <th>Price of AAPL</th>
+                                        <th>Action of GLD</th>
+                                        <th>Price of GLD</th>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                    </tbody>
-                                </table>
+                                    <tbody></tbody>
+                                    </table>
                             </div>
                         </div>
                     </div>
@@ -179,107 +178,142 @@ $(document).ready(function () {
         let lowerBandData = prepareData(response.dates, response.lower_band);
         let profitAndLossData = prepareData(response.dates, response.profit_and_loss);
 
-        // 準備信號數據
-        let buyStock1Signals = response.signals.filter(signal => signal.action_stock1 === 'BUY').map(signal => [new Date(signal.date).getTime(), signal.price_stock1]);
-        let sellStock1Signals = response.signals.filter(signal => signal.action_stock1 === 'SELL').map(signal => [new Date(signal.date).getTime(), signal.price_stock1]);
-        let buyStock2Signals = response.signals.filter(signal => signal.action_stock2 === 'BUY').map(signal => [new Date(signal.date).getTime(), signal.price_stock2]);
-        let sellStock2Signals = response.signals.filter(signal => signal.action_stock2 === 'SELL').map(signal => [new Date(signal.date).getTime(), signal.price_stock2]);
+        console.log(stock1Data);
+        console.log(stock2Data);
+        console.log(spreadData);
+        console.log(rollingMeanData);
+        console.log(upperBandData);
+        console.log(lowerBandData);
+        console.log(profitAndLossData);
+
+        // Prepare signals for marking
+        let buyAAPLSignals = response.signals.filter(signal => signal.action_aapl === 'BUY').map(signal => [new Date(signal.date).getTime(), signal.price_aapl]);
+        let sellAAPLSignals = response.signals.filter(signal => signal.action_aapl === 'SELL').map(signal => [new Date(signal.date).getTime(), signal.price_aapl]);
+
+        let buyGLDSignals = response.signals.filter(signal => signal.action_gld === 'BUY').map(signal => [new Date(signal.date).getTime(), signal.price_gld]);
+        let sellGLDSignals = response.signals.filter(signal => signal.action_gld === 'SELL').map(signal => [new Date(signal.date).getTime(), signal.price_gld]);
+
         let openSignals = response.signals.filter(signal => signal.type === 'OPEN').map(signal => [new Date(signal.date).getTime(), signal.profit_loss]);
         let closeSignals = response.signals.filter(signal => signal.type === 'CLOSE').map(signal => [new Date(signal.date).getTime(), signal.profit_loss]);
-
-        // 繪製價格和價差圖表
+        // Plot Price & Spread chart with signals
         Highcharts.stockChart('price-chart', {
-            title: { text: '價格與價差' },
-            xAxis: { type: 'datetime', ordinal: false },
-            yAxis: { title: { text: '價格' } },
+            title: { text: 'Price & Spread' },
+            xAxis: {
+                type: 'datetime',
+                ordinal: false
+            },
+            yAxis: { title: { text: 'Price' } },
             series: [{
-                name: trackDetails.stock1,
+                name: response.stock1,
                 data: stock1Data,
                 connectNulls: false
             }, {
-                name: trackDetails.stock2,
+                name: response.stock2,
                 data: stock2Data,
                 connectNulls: false
             }, {
-                name: trackDetails.stock1 + ' 買入信號',
+                name: 'AAPL Buy Signals',
                 type: 'scatter',
-                data: buyStock1Signals,
-                marker: { symbol: 'triangle', fillColor: 'green' }
+                data: buyAAPLSignals,
+                marker: {
+                    symbol: 'triangle',
+                    fillColor: 'green'
+                }
             }, {
-                name: trackDetails.stock1 + ' 賣出信號',
+                name: 'AAPL Sell Signals',
                 type: 'scatter',
-                data: sellStock1Signals,
-                marker: { symbol: 'triangle-down', fillColor: 'red' }
+                data: sellAAPLSignals,
+                marker: {
+                    symbol: 'triangle-down',
+                    fillColor: 'red'
+                }
             }, {
-                name: trackDetails.stock2 + ' 買入信號',
+                name: 'GLD Buy Signals',
                 type: 'scatter',
-                data: buyStock2Signals,
-                marker: { symbol: 'triangle', fillColor: 'green' }
+                data: buyGLDSignals,
+                marker: {
+                    symbol: 'triangle',
+                    fillColor: 'green'
+                }
             }, {
-                name: trackDetails.stock2 + ' 賣出信號',
+                name: 'GLD Sell Signals',
                 type: 'scatter',
-                data: sellStock2Signals,
-                marker: { symbol: 'triangle-down', fillColor: 'red' }
+                data: sellGLDSignals,
+                marker: {
+                    symbol: 'triangle-down',
+                    fillColor: 'red'
+                }
             }]
         });
 
-        // 繪製布林帶圖表
+        // Plot Bollinger Bands chart with signals
         Highcharts.stockChart('bollinger-chart', {
-            title: { text: '布林帶與信號' },
-            xAxis: { type: 'datetime', ordinal: false },
+            title: { text: 'Bollinger Bands & Signals' },
+            xAxis: {
+                type: 'datetime',
+                ordinal: false
+            },
             series: [{
-                name: '價差',
+                name: 'Spread',
                 data: spreadData,
                 connectNulls: false
             }, {
-                name: '移動平均線',
+                name: 'Rolling Mean',
                 data: rollingMeanData,
                 connectNulls: false
             }, {
-                name: '上限帶',
+                name: 'Upper Band',
                 data: upperBandData,
                 connectNulls: false
             }, {
-                name: '下限帶',
+                name: 'Lower Band',
                 data: lowerBandData,
                 connectNulls: false
-            }]
+            },]
         });
 
-        // 繪製利潤損失圖表
+        // Plot Profit & Loss chart
         Highcharts.stockChart('profit-chart', {
-            title: { text: '未實現利潤與損失' },
-            xAxis: { type: 'datetime' },
+            title: { text: 'Unrealized Profit & Loss' },
+            xAxis: {
+                type: 'datetime'
+            },
             series: [{
-                name: '利潤與損失',
+                name: 'Profit & Loss',
                 data: profitAndLossData
             }, {
-                name: '開倉點',
+                name: 'Entry Points',
                 type: 'scatter',
                 data: openSignals,
-                marker: { symbol: 'triangle', fillColor: 'green' }
+                marker: {
+                    symbol: 'triangle',
+                    fillColor: 'green'
+                }
             }, {
-                name: '平倉點',
+                name: 'Exit Points',
                 type: 'scatter',
                 data: closeSignals,
-                marker: { symbol: 'triangle-down', fillColor: 'red' }
+                marker: {
+                    symbol: 'triangle-down',
+                    fillColor: 'red'
+                }
             }]
         });
 
-        // 填充信號表格
+        // Populate table with signals
         let signalTableBody = $('#signal-table tbody');
         signalTableBody.empty();
         response.signals.forEach(signal => {
             signalTableBody.append(`
-                <tr>
-                    <td>${signal.date}</td>
-                    <td>${signal.type}</td>
-                    <td>${signal.action_stock1}</td>
-                    <td>${signal.price_stock1.toFixed(2)}</td>
-                    <td>${signal.action_stock2}</td>
-                    <td>${signal.price_stock2.toFixed(2)}</td>
-                </tr>
-            `);
+                 <tr>
+                     <td>${signal.date}</td>
+                     <td>${signal.type}</td>
+                     <td>${signal.action_aapl}</td>
+                     <td>${signal.price_aapl}</td>
+                     <td>${signal.action_gld}</td>
+                     <td>${signal.price_gld}</td>
+                 </tr>
+             `);
         });
 
         // 初始化或重新初始化 DataTable
@@ -287,23 +321,10 @@ $(document).ready(function () {
             $('#signal-table').DataTable().clear().rows.add(signalTableBody.children()).draw();
         } else {
             $('#signal-table').DataTable({
-                pageLength: 5,
-                lengthMenu: [[5, 10, 25, 50, -1], [5, 10, 25, 50, "全部"]],
+                pageLength: 5, // Show 5 entries by default
+                lengthMenu: [5, 10, 20, "All"],
                 responsive: true,
-                language: {
-                    "lengthMenu": "顯示 _MENU_ 條記錄",
-                    "zeroRecords": "沒有找到記錄",
-                    "info": "顯示第 _START_ 至 _END_ 項記錄，共 _TOTAL_ 項",
-                    "infoEmpty": "顯示第 0 至 0 項記錄，共 0 項",
-                    "infoFiltered": "(由 _MAX_ 項記錄過濾)",
-                    "search": "搜索:",
-                    "paginate": {
-                        "first": "首頁",
-                        "last": "末頁",
-                        "next": "下一頁",
-                        "previous": "上一頁"
-                    }
-                }
+                searching: true
             });
         }
     }
